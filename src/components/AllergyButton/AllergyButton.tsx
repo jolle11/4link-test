85cdea39a13b5b3c6e0c456398b45f50a3d2960d
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-07 17:16:07 +0100

    Deleted unnecessary lines

--- src/components/AllergyButton/AllergyButton.tsx
+++ src/components/AllergyButton/AllergyButton.tsx
@@ -9,9 +9,9 @@ interface IProps {
 const AllergyButton = ({ name, isShown, setIsShown }: IProps) => {
 	const [toggleUseEffect, setToggleUseEffect] = useState<boolean>(false);
 	useEffect(() => {
-		console.log("UseEffect");
 		setIsShown(!isShown);
 	}, [toggleUseEffect]);
+
 	return (
 		<button className="button__allergy" onClick={() => setToggleUseEffect(!toggleUseEffect)}>
 			{name}
