f2945b5ffc2b8c2bef2b4c46ddde85e2e7ef7640
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-07 17:14:47 +0100

    Added vite fonts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [
+		react(),
+		VitePluginFonts({
+			google: {
+				families: [
+					{
+						name: "Josefin Sans",
+						styles: "wght@700",
+						defer: true,
+						// font-family: 'Josefin Sans', sans-serif;
+					},
+					{
+						name: "Josefin Slab",
+						styles: "ital,wght@0,500;0,700;1,500;1,700",
+						defer: true,
+						// font-family: 'Josefin Slab', serif;
+					},
+				],
+			},
+		}),
+	],
+});
