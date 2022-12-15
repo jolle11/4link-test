9b8ce67f03d9dfebfe7eaee532fc9f3bce0011ff
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-15 10:43:18 +0100

    Wanted to add rtkq but endpoints were not working

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+// import { configureStore } from "@reduxjs/toolkit";
+
+// import { apiSlice } from "../features/activities/activityApiSlice";
+// import activityReducer from "../features/activities/activitySlice";
+
+// export const store = configureStore({
+// 	reducer: {
+// 		activity: activityReducer,
+// 		[apiSlice.reducerPath]: apiSlice.reducer,
+// 	},
+// 	middleware: (getDefaultMiddleware) => {
+// 		return getDefaultMiddleware().concat(apiSlice.middleware);
+// 	},
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
